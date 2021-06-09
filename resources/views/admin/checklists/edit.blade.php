81e5244af1bce1b6e868704531b15c52440de346
Ambush9 <ambush2626@gmail.com>
2021-06-09 14:38:02 +0300

    Checklist tasks, validation.Soring tasks with livewire. Ckeditor.

--- resources/views/admin/checklists/edit.blade.php
+++ resources/views/admin/checklists/edit.blade.php
@@ -16,7 +16,9 @@
                             </div>
                         @endif
 
-                        <form action="{{ route('admin.checklist_groups.checklists.update', [$checklistGroup, $checklist]) }}" method="POST">
+                        <form
+                            action="{{ route('admin.checklist_groups.checklists.update', [$checklistGroup, $checklist]) }}"
+                            method="POST">
                             @csrf
                             @method('PUT')
                             <div class="card-header">{{ __('Edit checklist') }}</div>
@@ -26,23 +28,85 @@
                                     <div class="col-sm-12">
                                         <div class="form-group">
                                             <label for="name">{{ __('Name') }}</label>
-                                            <input class="form-control" value="{{ $checklist->name }}" name="name" type="text">
+                                            <input class="form-control" value="{{ $checklist->name }}" name="name"
+                                                   type="text">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="card-footer">
-                                <button class="btn btn-sm btn-primary" type="submit"> {{ __('Save') }}</button>
+                                <button class="btn btn-sm btn-primary"
+                                        type="submit"> {{ __('Save checklist') }}</button>
                             </div>
                         </form>
                     </div>
-                    <form action="{{ route('admin.checklist_groups.checklists.destroy', [$checklistGroup, $checklist]) }}" method="POST">
+                    <form
+                        action="{{ route('admin.checklist_groups.checklists.destroy', [$checklistGroup, $checklist]) }}"
+                        method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')" type="submit"> {{ __('Delete this checklist') }}</button>
+                        <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"
+                                type="submit"> {{ __('Delete this checklist') }}</button>
                     </form>
+                    <hr/>
+
+                    <div class="card">
+                        <div class="card-header"><i class="fa fa-align-justify"></i> {{__('List of tasks')}}</div>
+                        <div class="card-body">
+                            @livewire('tasks-table', ['checklist' => $checklist])
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        @if ($errors->storetask->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->storetask->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form action="{{ route('admin.checklists.tasks.store', [$checklist]) }}" method="POST">
+                            @csrf
+                            <div class="card-header">{{ __('New task') }}</div>
+
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label for="name">{{ __('Name') }}</label>
+                                            <input class="form-control" name="name" value="{{ old('name') }}"
+                                                   type="text">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="description">{{ __('Description') }}</label>
+                                            <textarea class="form-control" id="task-textarea" name="description"
+                                                      rows="5">{{ old('description') }}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button class="btn btn-sm btn-primary" type="submit"> {{ __('Save task') }}</button>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
+
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#task-textarea' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
 @endsection
---
+++ resources/views/livewire/tasks-table.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <table class="table table-responsive-sm" wire:sortable="updateTaskOrder">
+        <tbody>
+        @foreach($tasks as $task)
+            <tr wire:sortable.item="{{ $task->id }}" wire:key="task-{{ $task->id }}">
+                <td>{{ $task->name }}</td>
+                <td>
+                    <a class="btn btn-sm btn-primary" href="{{ route('admin.checklists.tasks.edit', [$checklist, $task]) }}">{{ __('Edit') }}</a>
+                    <form style="display: inline-block"
+                          action="{{ route('admin.checklists.tasks.destroy', [$checklist, $task]) }}"
+                          method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-sm btn-danger"
+                                onclick="return confirm('Are you sure?')"
+                                type="submit"> {{ __('Delete') }}</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
